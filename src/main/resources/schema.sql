0fd3cfd9fbbbc869fbd34141dc60ce4217c8cd0a
pie <yejinh_h@naver.com>
2024-09-06 10:39:08 +0900

    docs : 스키마 및 도커 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+-- users
+CREATE TABLE IF NOT EXISTS users
+(
+    id         bigint auto_increment primary key comment 'ID',
+    user_token varchar(255) not null comment 'user_token',
+    login_id   varchar(10)  not null comment '로그인 아이디',
+    password   varchar(15)  not null comment '비밀번호'
+) comment 'users' charset = utf8;
+
+-- token
+create table tokens
+(
+    user_token varchar(255) not null comment 'user_token',
+    token      varchar(255) not null comment 'refresh_token'
+) comment 'tokens' charset = utf8;
