44083e91774dcbb5aa10194ab670d49122771b15
pie <yejinh_h@naver.com>
2024-09-07 14:41:15 +0900

    feat : user token을 위한 TokenGenerator 생성

---
+++ src/main/java/wanted/goldroom/authentication/infrastructure/common/util/TokenGenerator.java
@@ -0,0 +1,15 @@
+package wanted.goldroom.authentication.infrastructure.common.util;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class TokenGenerator {
+    private static final int TOKEN_LENGTH = 20;
+
+    private static String randomCharacter(int length) {
+        return RandomStringUtils.randomAlphanumeric(length);
+    }
+
+    public static String randomCharacterWithPrefix(String prefix) {
+        return prefix + randomCharacter(TOKEN_LENGTH - prefix.length());
+    }
+}
